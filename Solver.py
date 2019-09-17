bcc196f1d654639b52f1232c249650b36a88e660
Dipesh Makwana <dipesh_makwana@hotmail.co.uk>
2019-09-17 16:31:39 +0100

    Recursion Error Fixed

--- Solver.py
+++ Solver.py
@@ -1,13 +1,13 @@
 class Board:
-    board = [[0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0]]
+    board = [[7,8,0,4,0,0,1,2,0],
+            [6,0,0,0,7,5,0,0,9],
+            [0,0,0,6,0,1,0,7,8],
+            [0,0,7,0,4,0,2,6,0],
+            [0,0,1,0,5,0,9,3,0],
+            [9,0,4,0,6,0,0,0,5],
+            [0,7,0,3,0,0,0,1,2],
+            [1,2,0,0,0,7,4,0,0],
+            [0,4,9,2,0,6,0,0,7]]
     
     def print_Board(self):
         
@@ -30,12 +30,12 @@ class Board:
         for row in range(len(self.board)):
             for col in range(len(self.board[0])):
                 if self.board[row][col] == 0:
-                    return (row, col) 
+                    return (row, col)  # row, col
 
         return False
     
     def insert(self, num, pos):
-        self.board[pos[0][1]] = num
+        self.board[pos[0]][pos[1]] = num
     
 
 
@@ -48,15 +48,15 @@ class Solver():
 
     def solve(self):
         
-        if not self.board.findEmpty():
+        if not self.board.find_Empty():
             return True
         else:
-            row, col = self.board.findEmpty()
-        for val in range(0, 10):
-            if valid(val, (row, col)):
+            row, col = self.board.find_Empty()
+        for val in range(1, 10):
+            if self.valid(val, (row, col)):
                 self.board.insert(val, (row, col))
 
-                if solve():
+                if self.solve():
                     return True
                 
                 self.board.insert(0, (row, col))
@@ -67,12 +67,13 @@ class Solver():
 
     def valid(self, num, pos):
 
-        for row in range(len(self.board[0])):
-            if self.board[pos[0][row]] == num and pos[1] != row:
-                return False
+        for row in range(len(self.board.board[0])):
+            if self.board.board[pos[0]][row] == num:
+                if pos[1] != row:
+                    return False
 
-        for col in range(len(self.board)):
-            if self.board[col][pos[1]] == num and pos[0] != col:
+        for col in range(len(self.board.board)):
+            if self.board.board[col][pos[1]] == num and pos[0] != col:
                 return False       
 
         box_X = pos[1] // 3
@@ -80,7 +81,7 @@ class Solver():
 
         for row in range(box_Y * 3, box_Y * 3 + 3):
             for col in range(box_X * 3, box_X * 3 + 3):
-                if self.board[row][col] == num and (row, col) != pos:
+                if self.board.board[row][col] == num and (row, col) != pos:
                     return False
 
         return True
@@ -92,5 +93,16 @@ class Solver():
 
        
     
-Board().print_Board() 
+board = Board()
+
+print("Sudoku")
+board.print_Board()
+print( " ")
+
+solver = Solver(board).solve()
+
+print("Solution")
+board.print_Board()
+
+
  
